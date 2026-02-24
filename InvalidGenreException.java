d9eaa0b48d97e3fdd9fb98ee7e118245dd1a6faa
Gaurang Surte <88529949+GAURANGSURTE@users.noreply.github.com>
2026-02-24 13:46:04 +0530

    Add files via upload

---
+++ InvalidGenreException.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+// InvalidGenreException
+public class InvalidGenreException extends Exception{
+    public InvalidGenreException(String msg)
+    {
+        super(msg);
+    }
+}
+
+//InvalidPriceException
+import java.util.*;
+public class InvalidPriceException extends Exception{
+    public InvalidPriceException(String msg)
+    {
+        super(msg);
+    }
+}
